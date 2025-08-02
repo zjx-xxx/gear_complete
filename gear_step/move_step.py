c479de1ef4411202b373d805f6d7f1dad8acd19b
Lee-ooo <listyx.w@gmail.com>
2025-08-02 20:28:59 +0800

    提供两个齿轮

--- gear_step/move_step.py
+++ gear_step/move_step.py
@@ -3,7 +3,7 @@ import shutil
 from datetime import datetime
 
 # 原始文件路径
-original_file = 'assembled_gear_pair.step'
+original_file_1 = '../Macro/SpurGear1_cut.step'
 
 # 获取当前时间，格式为：年月日小时分钟
 timestamp = datetime.now().strftime('%Y%m%d%H%M')
@@ -13,13 +13,28 @@ target_dir = os.path.join('..', 'output_step')
 os.makedirs(target_dir, exist_ok=True)
 
 # 构建目标文件名
-base_name, ext = os.path.splitext(original_file)
-new_filename = f"{base_name}_{timestamp}{ext}"
+base_name, ext = os.path.splitext(original_file_1)
+new_filename = f"{base_name}_{timestamp}{ext}-1"
 
 # 构建完整目标路径
 target_path = os.path.join(target_dir, new_filename)
 
 # 执行移动操作
-shutil.move(original_file, target_path)
+shutil.move(original_file_1, target_path)
+print(f"齿轮1文件已移动到: {target_path}")
 
-print(f"文件已移动到: {target_path}")
+# 原始文件路径
+original_file_2 = '../Macro/SpurGear2_cut.step'
+
+# 构建目标文件名
+base_name, ext = os.path.splitext(original_file_2)
+new_filename = f"{base_name}_{timestamp}{ext}-2"
+
+# 构建完整目标路径
+target_path = os.path.join(target_dir, new_filename)
+
+# 执行移动操作
+shutil.move(original_file_2, target_path)
+
+
+print(f"齿轮2文件已移动到: {target_path}")
